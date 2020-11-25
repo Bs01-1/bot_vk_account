a83296656f643ec87d0a54574829cbd6947ce416
Bs01-1 <kkutukov.ilya@mail.ru>
2020-11-25 17:10:10 +0500

    patch 3

--- models/Sessions.js
+++ models/Sessions.js
@@ -1,6 +1,6 @@
 
 module.exports = class Sessions {
     static checkTimeExit (user, type) {
-        return connect.promise().query('SELECT * from ' + table)
+        return connect.promise().query('SELECT * from ' + table);
     }
 }
