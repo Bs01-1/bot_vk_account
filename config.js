fc216cc60cf1dcc573b8e40d91af0a1f8a7d404d
Bs01-1 <kkutukov.ilya@mail.ru>
2020-12-01 01:01:44 +0500

    patch 3.1

--- config.js
+++ config.js
@@ -6,9 +6,8 @@ module.exports.settings = {
         database: '',
         password: '',
     },
-    time_zone: 5, // Time Zone 0 == 0GMT (London)
+    time_zone: 0, // Time Zone 0 == 0GMT (London)
     path: '',
-    start_send: true,
     iris_conversation: '',
     iris_conversation_link: ''
 };
@@ -18,4 +17,5 @@ module.exports.settings = {
  * @type {{id: string, config: {type: [string, number or array], key: string or message: string or array, peer_id: string}}[]}
  */
 module.exports.message = [
+
 ];
